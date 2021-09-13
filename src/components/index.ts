b483254463c1f77e86a299a90ca64edd0f529a66
rishi-org-stack <rishistack17@gmail.com>
2021-09-13 17:33:07 +0530

    feat(components): added index file

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+import Wrapper from './wrapper';
+import Input from './input';
+import Button from './button';
+import Text from './text';
+
+export {
+    Wrapper,
+    Input,
+    Button,
+    Text
+}
